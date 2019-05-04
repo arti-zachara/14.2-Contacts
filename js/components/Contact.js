1a473fac9bfdf9568c532028038ffe50516e0a89
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-04 19:20:32 +0200

    Add styles and additional div-label

--- js/components/Contact.js
+++ js/components/Contact.js
@@ -12,19 +12,26 @@ var Contact = React.createClass({
         src: "http://icons.veryicon.com/png/System/Micro/User.png"
       }),
       React.createElement(
-        "p",
-        { className: "contactLabel" },
-        "Imię: " + this.props.item.firstName
-      ),
-      React.createElement(
-        "p",
-        { className: "contactLabel" },
-        "Nazwisko: " + this.props.item.lastName
-      ),
-      React.createElement(
-        "a",
-        { className: "contactEmail", href: "mailto:" + this.props.item.email },
-        this.props.item.email
+        "div",
+        { className: "label" },
+        React.createElement(
+          "p",
+          { className: "contactLabel" },
+          "Imię: " + this.props.item.firstName
+        ),
+        React.createElement(
+          "p",
+          { className: "contactLabel" },
+          "Nazwisko: " + this.props.item.lastName
+        ),
+        React.createElement(
+          "a",
+          {
+            className: "contactEmail",
+            href: "mailto:" + this.props.item.email
+          },
+          this.props.item.email
+        )
       )
     );
   }
