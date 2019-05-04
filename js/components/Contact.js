19cc9cf978a9ea3ff31d13bfe35d5f5733dfda24
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-05 00:13:35 +0200

    Add babel and transform js scripts  to JSX

--- js/components/Contact.js
+++ js/components/Contact.js
@@ -4,35 +4,29 @@ var Contact = React.createClass({
   },
 
   render: function() {
-    return React.createElement(
-      "div",
-      { className: "contactItem" },
-      React.createElement("img", {
-        className: "contactImage",
-        src: "http://icons.veryicon.com/png/System/Micro/User.png"
-      }),
-      React.createElement(
-        "div",
-        { className: "label" },
-        React.createElement(
-          "p",
-          { className: "contactLabel" },
-          "Imię: " + this.props.item.firstName
-        ),
-        React.createElement(
-          "p",
-          { className: "contactLabel" },
-          "Nazwisko: " + this.props.item.lastName
-        ),
-        React.createElement(
-          "a",
-          {
-            className: "contactEmail",
-            href: "mailto:" + this.props.item.email
-          },
-          this.props.item.email
-        )
-      )
+    return (
+      <div className={"contactItem"}>
+        <img
+          className={"contactImage"}
+          src={"http://icons.veryicon.com/png/System/Micro/User.png"}
+        />
+        <div className={"label"}>
+          <p className={"contactLabel"}>Imię: {this.props.item.firstName}</p>
+          <p className={"contactLabel"}>Nazwisko: {this.props.item.lastName}</p>
+          <a href={"mailto:" + this.props.item.email}>
+            {this.props.item.email}
+          </a>
+        </div>
+      </div>
     );
+    // without babel
+    // React.createElement("div", { className: "contactItem" },
+    //   React.createElement("img", { className: "contactImage", src: "http://icons.veryicon.com/png/System/Micro/User.png"}),
+    //   React.createElement("div", { className: "label" },
+    //     React.createElement("p", { className: "contactLabel" }, "Imię: " + this.props.item.firstName),
+    //     React.createElement("p", { className: "contactLabel" }, "Nazwisko: " + this.props.item.lastName),
+    //     React.createElement("a", {className: "contactEmail", href: "mailto:" + this.props.item.email}, this.props.item.email)
+    //   )
+    // );
   }
 });
